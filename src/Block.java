f81b4aa37b41a5421d963b1b32ca6139b6b54839
devflamenbaum <bielflam.engpet@gmail.com>
2024-03-28 23:31:34 -0300

    mine my first blockchain

--- src/Block.java
+++ src/Block.java
@@ -7,6 +7,8 @@ public class Block {
     private String data;
     private long timeStamp;
 
+    private int nounce;
+
     public Block(String data, String previousHash){
         this.data = data;
         this.previousHash = previousHash;
@@ -15,6 +17,17 @@ public class Block {
     }
 
     public String calculateHash(){
-        return StringUtil.applySha256(previousHash + Long.toString(timeStamp) + data);
+        return StringUtil.applySha256(
+                previousHash + timeStamp + nounce + data
+        );
+    }
+
+    public void mineBlock(int difficulty){
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while(!hash.substring( 0, difficulty).equals(target)){
+            nounce++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
     }
 }
--- src/MyChain.java
+++ src/MyChain.java
@@ -5,15 +5,50 @@ import java.util.ArrayList;
 public class MyChain {
 
     public static ArrayList<Block> blockchain = new ArrayList<>();
+    public static int difficulty = 5;
 
     public static void main(String[] args) {
 
+        Long startTime = System.currentTimeMillis();
+
         blockchain.add(new Block("First block", "0"));
+        System.out.println("Trying to mine block 1...");
+        blockchain.get(0).mineBlock(difficulty);
         blockchain.add(new Block("Second block", blockchain.get(blockchain.size() - 1).hash));
+        System.out.println("Trying to mine block 2...");
+        blockchain.get(1).mineBlock(difficulty);
         blockchain.add(new Block("Third block", blockchain.get(blockchain.size() - 1).hash));
+        System.out.println("Trying to mine block 3...");
+        blockchain.get(2).mineBlock(difficulty);
+
+        Long endTime = System.currentTimeMillis();
+
+        System.out.println("\nTime taken to mine 3 blocks in difficulty " + difficulty + ": " + (endTime - startTime)/1000 + " s");
+
+        System.out.println("\nBlockchain is valid: " + isChainValid());
 
         String blockChainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
 
+        System.out.println("\nBlockchain: ");
         System.out.println(blockChainJson);
+    }
+
+    public static Boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+
+        for (int i = 1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i - 1);
+            if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                System.out.println("Current Hashes not equal");
+                return false;
+            }
+            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+                System.out.println("Previous Hashes not equal");
+                return false;
+            }
+        }
+        return true;
     }
 }
