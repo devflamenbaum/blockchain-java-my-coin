027eafd66e83fa73331d3f5ef1e4542f3c062ba9
devflamenbaum <bielflam.engpet@gmail.com>
2024-03-28 22:58:22 -0300

    create simple blockchain & lib external

---
+++ src/MyChain.java
@@ -0,0 +1,19 @@
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+
+public class MyChain {
+
+    public static ArrayList<Block> blockchain = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        blockchain.add(new Block("First block", "0"));
+        blockchain.add(new Block("Second block", blockchain.get(blockchain.size() - 1).hash));
+        blockchain.add(new Block("Third block", blockchain.get(blockchain.size() - 1).hash));
+
+        String blockChainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+
+        System.out.println(blockChainJson);
+    }
+}
